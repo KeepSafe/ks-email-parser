1c0320b1dcc59e42aa84ce8ce7c85cb3dc18b5a6
ca77y <ca77y@live.com>
2014-12-12 00:50:36 +0100

    match any url pattern

--- email_parser/markdown_ext.py
+++ email_parser/markdown_ext.py
@@ -21,6 +21,14 @@ class InlineBlockProcessor(BlockProcessor):
 
 
 class BaseUrlImagePattern(Pattern):
+    url_pattern = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
     def __init__(self, images_dir, *args):
         super().__init__(*args)
         if images_dir:
@@ -29,11 +37,15 @@ class BaseUrlImagePattern(Pattern):
             self.images_dir = ''
         self.image_pattern = ImagePattern(*args)
 
+    def _is_url(self, text):
+        url = text.strip().strip('/').split(' ')[0]
+        return self.url_pattern.match(url)
+
     def handleMatch(self, m):
-        if not m.group(10).strip('/').startswith('http'):
-            image = IMAGE_PATTERN.format(m.group(2), self.images_dir, m.group(10).strip('/'))
-        else:
+        if self._is_url(m.group(10)):
             image = m.string
+        else:
+            image = IMAGE_PATTERN.format(m.group(2), self.images_dir, m.group(10).strip('/'))
         pattern = re.compile("^(.*?)%s(.*?)$" % self.image_pattern.pattern, re.DOTALL | re.UNICODE)
         match = re.match(pattern, ' ' + image + ' ')
         return self.image_pattern.handleMatch(match)
