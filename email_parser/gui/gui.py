70eb243b064aab17e93d8d7e0764467123f7ffa6
Kevin Saff <kcsaff@zhook.local>
2015-10-01 19:58:19 -0700

    Browse and edit completed emails.

--- email_parser/gui/gui.py
+++ email_parser/gui/gui.py
@@ -4,12 +4,14 @@ import http.server
 import os, os.path
 from .. import fs
 from ..renderer import HtmlRenderer
-from ..reader import Template
+from ..reader import Template, read as reader_read
 import fnmatch
 import cherrypy
 
 
 STYLES_PARAM_NAME = 'styles'
+EDIT_PARAM_NAME = 'edit'
+EDITED_PARAM_NAME = 'edited'
 
 
 def soup_fragment(html_fragment):
@@ -21,6 +23,94 @@ def soup_fragment(html_fragment):
         return soup.html.next
     else:
         return soup
+
+
+def _make_fields(names, values):
+    result = list()
+    if names:
+        result.append('<fieldset class="generated-fields"><ul>')
+        for name in names:
+            result.append((
+                '<li><label>{0}: ' +
+                '<input type="text" name="{0}" placeholder="{0}" value="{1}" style="width: 60%" />' +
+                '</label></li>').format(
+                name, values.get(name, '')
+            ))
+        result.append('</ul></fieldset>')
+    return '\n'.join(result)
+
+
+def _make_hidden_fields(*args):
+    result = list()
+    result.append('<div class="generated-hidden">')
+    for values in args:
+        for name, value in values.items():
+            result.append('<input type="hidden" name="{0}" value="{1}"/>'.format(name, value))
+    result.append('</div>')
+    return '\n'.join(result)
+
+
+def _make_actions(items):
+    result = list()
+    if items:
+        result.append('<fieldset class="generated-actions">')
+        for name, action in items:
+            result.append('<input type="submit" value="{0}" formaction="{1}" />'.format(name, action))
+        result.append('</fieldset>')
+    return '\n'.join(result)
+
+
+def _directory(description, root, path, href, accepts=(lambda name: not name.startswith('.'))):
+
+    full_path = os.path.join(root, path)
+    soup = bs4.BeautifulSoup('''<html>
+<head><title>Template index of {0}</title></head>
+<body>
+<h1>Contents of <span class="index-path">{0}</span></h1>
+<ul>
+</ul>
+</body>
+</html>'''.format(description))
+    ul = soup.find('ul')
+    for name in sorted(os.listdir(full_path)):
+        if accepts(name):
+            name_path = os.path.join(path, name)
+            ul.append(soup_fragment('<li><a href="{href}">{name}</a></li>'.format(
+                href=href(name_path), name=name
+            )))
+    return soup.prettify()
+
+
+def _wrap_body_in_form(html, prefixes=[], postfixes=[]):
+    soup = bs4.BeautifulSoup(html)
+    body = soup.find('body')
+    new_form = soup.new_tag('form', **{'method': 'POST'})
+    for content in reversed(body.contents):
+        new_form.insert(0, content.extract())
+    for prefix in reversed(prefixes):
+        if prefix.strip():
+            new_form.insert(0, soup_fragment(prefix))
+    for postfix in postfixes:
+        if postfix.strip():
+            new_form.append(soup_fragment(postfix))
+
+    body.append(new_form)
+    return str(soup)
+
+
+def _make_subject_line(subject):
+    return '<h1 class="subject" style="text-align: center">{}</h1>'.format(subject)
+
+
+def _unplaceholder(placeholders):
+    def fix_item(item):
+        if item.startswith('[[') and item.startswith(']]'):
+            return item[2:-2]
+        else:
+            return item
+    X = {K: fix_item(V) for K, V in placeholders.items()}
+    print(X)
+    return X
 
 
 class InlineFormReplacer(object):
@@ -37,7 +127,7 @@ class InlineFormReplacer(object):
         self.attrs = list()
         self.required = list()
 
-    def get(self, name):
+    def require(self, name):
         if name not in self.names:
             self.names.append(name)
         if self.values.get(name):
@@ -58,14 +148,13 @@ class InlineFormReplacer(object):
             return before + self._textarea(name)
         elif '"' in (prefix, postfix):
             self.attrs.append(name)
-            return before + (self.get(name) or ('{{' + name + '}}'))
+            return before + (self.require(name) or ('{{' + name + '}}'))
         else:
             return before + self._textarea(name)
 
     def _textarea(self, name):
         return ('<textarea name="{0}" placeholder="{0}"' +
-                ' style="resize: vertical; width: 95%; height: 160px;">{1}</textarea>'.format(name, self.get(name))
-                )
+                ' style="resize: vertical; width: 95%; height: 160px;">{1}</textarea>').format(name, self.require(name))
 
     def replace(self, template_html):
         return self.CONTENT_REGEX.sub(self._sub, template_html)
@@ -74,24 +163,7 @@ class InlineFormReplacer(object):
         return {
             K: '[[{0}]]'.format(V) if K in self.attrs else V
             for K, V in self.values.items()
-            }
-
-    def _generate_fields(self, names=None, classname='names'):
-        result = list()
-        if names:
-            result.append('<fieldset class="{0}"><ul>'.format(classname))
-            for name in names:
-                result.append('    <li><label>{0}: <input type="text" name="{0}" placeholder="{0}" value="{1}"/></label></li>'
-                              .format(name, self.values.get(name, ''))
-                              )
-            result.append('</ul></fieldset>')
-        return '\n'.join(result)
-
-    def name_fields(self, extra_fields=()):
-        return self._generate_fields(list(extra_fields) + list(self.names))
-
-    def attr_fields(self, extra_fields=()):
-        return self._generate_fields(list(extra_fields) + list(self.attrs))
+        }
 
 
 class InlineFormRenderer(object):
@@ -100,22 +172,6 @@ class InlineFormRenderer(object):
 
     def _read_template(self, template_name):
         return fs.read_file(self.settings.templates, template_name)
-
-    def _wrap_body_in_form(self, html, prefixes=[], postfixes=[]):
-        soup = bs4.BeautifulSoup(html)
-        body = soup.find('body')
-        new_form = soup.new_tag('form', **{'method': 'GET'})
-        for content in reversed(body.contents):
-            new_form.insert(0, content.extract())
-        for prefix in reversed(prefixes):
-            if prefix.strip():
-                new_form.insert(0, soup_fragment(prefix))
-        for postfix in postfixes:
-            if postfix.strip():
-                new_form.append(soup_fragment(postfix))
-
-        body.append(new_form)
-        return str(soup)
 
     def _style_list(self, styles=(), path_glob='*.css'):
         result = list()
@@ -132,71 +188,141 @@ class InlineFormRenderer(object):
         else:
             return ''
 
-    def _index(self, path):
-        template_path = os.path.join(self.settings.templates, path)
-        soup = bs4.BeautifulSoup('''<html>
-<head><title>Template index of {0}</title></head>
-<body>
-    <h1>Contents of <span class="index-path">{0}</span></h1>
-    <ul>
-    </ul>
-</body>
-</html>'''.format(path or 'template directory'))
-        ul = soup.find('ul')
-        for name in sorted(os.listdir(template_path)):
-            if not name.startswith('.'):
-                name_path = os.path.join(path, name)
-                ul.append(soup_fragment('<li><a href="/template/{path}">{name}</a></li>'.format(
-                    path=name_path, name=name
-                )))
-        return soup.prettify()
+    def render(self, template_name, styles=(), force_edit=False,
+               editing_actions=[],
+               preview_actions=[],
+               **args
+               ):
+        replacer = InlineFormReplacer({'base_url': self.settings.images}, args)
+        replacer.require('subject')
+        template_html = self._read_template(template_name)
+        html = replacer.replace(template_html)
 
-    def render(self, template_name=None, styles=(), **vars):
-        if template_name:
-            template_path = os.path.join(self.settings.templates, template_name)
-        else:
-            template_path = self.settings.templates
-        if os.path.isdir(template_path):
-            return self._index(template_name)
-        else:
-            replacer = InlineFormReplacer({'base_url': self.settings.images}, vars)
-            replacer.get('subject')
-            template_html = self._read_template(template_name)
-            html = replacer.replace(template_html)
-            subject_line = '<h1 class="subject" style="text-align: center">{}</h1>'.format(vars.get('subject'))
-            html = self._wrap_body_in_form(
+        if force_edit or replacer.required or not styles:
+            # Some things are missing, show form with stuff still required
+            html = _wrap_body_in_form(
                 html,
                 prefixes=[
                     self._style_list(styles),
-                    replacer.attr_fields(['subject']),
-                    subject_line
+                    _make_fields(['subject'] + list(replacer.attrs), args),
+                    _make_subject_line(args.get('subject'))
                 ],
-                postfixes=['<fieldset><input type="submit" value="Render" /></fieldset>']
+                postfixes=[
+                    _make_actions(editing_actions)
+                ]
             )
-
-            if replacer.required:
-                print(replacer.required)
-                return html
-            else:  # Everything filled in, use "real" renderer
-                placeholders = replacer.placeholders()
-                print(placeholders)
-                html = HtmlRenderer(Template(template_name, styles), self.settings, '').render(placeholders)
-                return self._wrap_body_in_form(
-                    html,
-                    prefixes=[subject_line],
-                    postfixes=['<fieldset><input type="submit" value="Save" /></fieldset>']
-                )
+            return html
+        else:
+            # Everything filled in, use "real" renderer
+            placeholders = replacer.placeholders()
+            html = HtmlRenderer(Template(template_name, styles), self.settings, '').render(placeholders)
+            return _wrap_body_in_form(
+                html,
+                prefixes=[
+                    _make_subject_line(args.get('subject'))
+                ],
+                postfixes=[
+                    _make_hidden_fields(args, {STYLES_PARAM_NAME: styles[0]}),
+                    _make_actions(preview_actions)
+                ]
+            )
 
 
 class Server(object):
-    def __init__(self, renderer):
+    def __init__(self, settings, renderer):
+        self.settings = settings
         self.renderer = renderer
 
+    def _edit_template(self, name):
+        return '/template/{0}?{1}=1'.format(name, EDIT_PARAM_NAME)
+
+    def _edit_email(self, name):
+        return '/email/{0}?{1}=1'.format(name, EDIT_PARAM_NAME)
+
+    def _edited_email(self, name):
+        return '/email/{0}?{1}=1'.format(name, EDITED_PARAM_NAME)
+
+    def _editing_email(self, name):
+        return '/email/{0}?{1}=1&{2}=1'.format(name, EDIT_PARAM_NAME, EDITED_PARAM_NAME)
+
     @cherrypy.expose
-    def template(self, *paths, **vars):
-        path = '/'.join(paths)
-        styles = [vars.pop(STYLES_PARAM_NAME)] if STYLES_PARAM_NAME in vars else []
-        return self.renderer.render(path, styles, **vars)
+    def template(self, *paths, **args):
+        template_name = '/'.join(paths)
+        template_path = os.path.join(self.settings.templates, template_name)
+        if os.path.isdir(template_path):
+            return _directory(template_name or 'template directory',
+                              self.settings.templates, template_name, '/template/{}'.format)
+        else:  # A file
+            styles = [args.pop(STYLES_PARAM_NAME)] if STYLES_PARAM_NAME in args else []
+            force_edit = args.pop(EDIT_PARAM_NAME, False)
+            return self.renderer.render(template_name, styles, force_edit,
+                                        editing_actions=[
+                                            ['Preview', '/template/{}'.format(template_name)],
+                                        ],
+                                        preview_actions=[
+                                            ['Save', '/saveas/{}'.format(template_name)],
+                                            ['Edit', self._edit_template(template_name)],
+                                        ],
+                                        **args)
+
+    @cherrypy.expose
+    def saveas(self, *template_paths, **args):
+        template_name = '/'.join(template_paths)
+        raise NotImplemented
+        # Request path to save under, save it, & forward to complete rendered email
+
+    @cherrypy.expose
+    def save(self, *email_paths, **args):
+        email_name = '/'.join(email_paths)
+        raise NotImplemented
+        # Choose to overwrite or save as
+
+    @cherrypy.expose
+    def email(self, *paths, **args):
+        email_name = '/'.join(paths)
+        email_path = os.path.join(self.settings.source, email_name)
+        if os.path.isdir(email_path):
+            return _directory(email_name or 'source directory',
+                              self.settings.source, email_name, '/email/{}'.format)
+        else:  # A file
+            force_edit = args.pop(EDIT_PARAM_NAME, False)
+            was_edited = args.pop(EDITED_PARAM_NAME, False)
+            template, placeholders, _ = reader_read(email_path)
+            edited_args = _unplaceholder(placeholders)
+            if was_edited:
+                edited_args.update(args)
+            styles = [edited_args.pop(STYLES_PARAM_NAME)] if STYLES_PARAM_NAME in edited_args else template.styles
+            print(styles)
+            print(template.styles)
+
+            if force_edit or was_edited:
+                return self.renderer.render(template.name, styles, force_edit,
+                                            editing_actions=[
+                                                ['Preview', self._edited_email(email_name)],
+                                                ['Edit', self._editing_email(email_name)],
+                                                ['Reset', self._edit_email(email_name)],
+                                            ],
+                                            preview_actions=[
+                                                ['Save', '/save/{}'.format(email_name)],
+                                                ['Edit', self._editing_email(email_name)],
+                                                ['Reset', '/email/{}'.format(email_name)],
+                                            ],
+                                            **edited_args
+                                            )
+            else:  # Show existing email
+                html = HtmlRenderer(template, self.settings, '').render(placeholders)
+                return _wrap_body_in_form(
+                    html,
+                    prefixes=[
+                        _make_subject_line(edited_args.get('subject'))
+                    ],
+                    postfixes=[
+                        _make_hidden_fields(edited_args, {STYLES_PARAM_NAME: template.styles[0]}),
+                        _make_actions([
+                                ['Edit', self._edit_email(email_name)],
+                        ])
+                    ]
+                )
 
 
 def render(args):
@@ -206,7 +332,7 @@ def render(args):
     renderer = InlineFormRenderer(settings)
     if args.port:
         cherrypy.config.update({'server.socket_port': args.port})
-        cherrypy.quickstart(Server(renderer), '/')
+        cherrypy.quickstart(Server(settings, renderer), '/')
 
     elif args.template:
         print(renderer.render(args.template))
